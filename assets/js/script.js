e7cd14155148b52ff920a9850146ed0de5486d9f
angyanisa <ang.yanisa@gmail.com>
2024-08-07 11:12:54 -0500

    tried commenting out script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 // testimonials variables
+/*
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
@@ -52,7 +53,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
+*/
 
 
 // custom select variables
